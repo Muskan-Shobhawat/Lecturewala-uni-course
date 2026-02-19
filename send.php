3af9d7229ad2fed07ad36dec9ebedbaee47d1419
Muskan Shobhawat <muskanshobhawat123@gmail.com>
2026-02-19 13:16:02 +0530

    Edits commit

---
+++ send.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $course = htmlspecialchars($_POST["course"]);
+
+    $to = "muskanshobhawat123@gmail.com";
+    $subject = "New Online Course Enquiry";
+
+    $message = "
+    Name: $name\n
+    Email: $email\n
+    Phone: $phone\n
+    Course: $course\n
+    ";
+
+    $headers = "From: noreply@yourdomain.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Success";
+    } else {
+        echo "Error";
+    }
+}
+?>
